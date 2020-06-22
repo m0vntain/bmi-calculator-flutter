be4d10f5318f1bc6a3bc35129c83412e9b25c098
m0vntain <jmcdador@gmail.com>
2020-06-22 08:04:53 +0800

    create custom card content

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+const reusableCardColor = Color(0xFF1D1E33);
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,63 +14,106 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BMI CALCULATOR'),
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: reusableCardColor,
+                    cardChild: CustomIconContent(),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: reusableCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: reusableCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: reusableCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: reusableCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomIconContent extends StatelessWidget {
+  const CustomIconContent({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          FontAwesomeIcons.mars,
+          size: 80.0,
         ),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    Color(0xFF1D1E33),
-                  ),
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    Color(0xFF1D1E33),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    Color(0xFF1D1E33),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    Color(0xFF1D1E33),
-                  ),
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    Color(0xFF1D1E33),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ));
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          'Male',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
   }
 }
 
 class ReusableCard extends StatelessWidget {
-//  const ReusableCard({Key key,}) : super(key: key);
-
-  ReusableCard(this.colour);
+  ReusableCard({this.colour, this.cardChild});
 
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       height: 200.0,
       width: 170.0,
